95b6c68a49d3e5231be5f233038acdca9efb22ed
Сергей Бухаров <s.buharov@dicoming.com>
2015-06-08 15:03:22 +0300

    add CompositePropertyTest

---
+++ tests/unit/entity/property/CompositePropertyTest.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace unit\entity\property;
+
+
+use dicom\workflow\entity\property\CompositeProperty;
+use dicom\workflow\entity\property\Property;
+use dicom\workflow\rules\AlwaysTrueRule;
+use dicom\workflow\rules\GreaterThan;
+
+class CompositePropertyTest extends \PHPUnit_Framework_TestCase
+{
+    protected function createTestGirl()
+    {
+        $boobsSize = new Property('boobsSize');
+        $greaterThenRule = new GreaterThan();
+        $greaterThenRule->setConfig(2);
+        $boobsSize->addPropertyRule($greaterThenRule);
+
+        $prettyFace = new Property('prettyFace');
+        $prettyFace->addPropertyRule(new AlwaysTrueRule());
+
+        $girl = new CompositeProperty('girl');
+        $girl->addProperty($boobsSize);
+        $girl->addProperty($prettyFace);
+
+        return $girl;
+    }
+
+    public function testAddProperty()
+    {
+        $girl = $this->createTestGirl();
+        $this->arrayHasKey('boobsSize', $girl->getProperties());
+        $this->arrayHasKey('prettyFace', $girl->getProperties());
+    }
+
+    public function testExecutionIsTrue()
+    {
+        $girl = $this->createTestGirl();
+
+        $anna = [
+            'boobsSize' => 3,
+            'prettyFace' => 'yes'
+        ];
+
+        $executionResult = $girl->executeRules($anna, $anna);
+        $this->assertTrue($executionResult->isSuccess());
+    }
+
+    public function testExecutionIsFalse()
+    {
+        $girl = $this->createTestGirl();
+
+        $jane = [
+            'boobsSize' => 1,
+            'prettyFace' => 'yes'
+        ];
+
+        $executionResult = $girl->executeRules($jane, $jane);
+        $this->assertFalse($executionResult->isSuccess());
+    }
+}
