9e849c7f74e9311ebb458a9d692139251a7178a8
Гизатуллин Ринат <rgizatullin@kirkazan.ru>
2015-06-30 17:56:08 +0300

    часть интеграционных тестов

--- tests/integration/CookingTest.php
+++ tests/integration/CookingTest.php
@@ -40,69 +40,94 @@ class CookingTest extends \PHPUnit_Framework_TestCase
      *
      * @return true
      */
-//    public function testCookingBakingPie()
-//    {
-//        $rawPie = [
-//            'id' => 1,
-//            'stuffing' => 'cherry',
-//            'pastry' => 'yeast dough',
-//            'baking_time' => 0,
-//        ];
-//
-//        $bakedPie = [
-//            'id' => 1,
-//            'stuffing' => 'cherry',
-//            'pastry' => 'yeast dough',
-//            'baking_time' => 50,
-//        ];
-//
-//        $transitionResult = $this->engine->makeTransition('new', 'baked', $bakedPie, $rawPie);
-//        $this->assertTrue($transitionResult->isSuccess());
-//    }
-
-    /**
-     * Попытка изменить параметр который readonly
-     *
-     * @return false
-     */
-//    public function testCookingChangeStuffing()
-//    {
-//        $rawPie = [
-//            'id' => 1,
-//            'stuffing' => 'cherry',
-//            'pastry' => 'yeast dough',
-//            'baking_time' => 0,
-//        ];
-//
-//        $bakedPie = [
-//            'id' => 1,
-//            'stuffing' => 'strawberry', // меняем параметр, который нельзя менять.
-//                                        // в результате транзакция не должна быть выполнена
-//            'pastry' => 'yeast dough',
-//            'baking_time' => 50,
-//        ];
-//
-//        $transitionResult = $this->engine->makeTransition('new', 'baked', $bakedPie, $rawPie);
-//        $this->assertFalse($transitionResult->isSuccess());
-//    }
-
-    public function testCookingBurntPie()
+    public function testCookingBakingPie()
     {
         $rawPie = [
             'id' => 1,
             'stuffing' => 'cherry',
             'pastry' => 'yeast dough',
-//            'baking_time' => 0,
+            'baking_time' => 0,
         ];
 
         $bakedPie = [
             'id' => 1,
             'stuffing' => 'cherry',
             'pastry' => 'yeast dough',
-            'baking_time' => 60,
+            'baking_time' => 50,
         ];
 
         $transitionResult = $this->engine->makeTransition('new', 'baked', $bakedPie, $rawPie);
         $this->assertTrue($transitionResult->isSuccess());
+    }
+
+    /**
+     * Попытка изменить параметр который readonly
+     *
+     * @return false
+     */
+    public function testCookingChangeStuffing()
+    {
+        $rawPie = [
+            'id' => 1,
+            'stuffing' => 'cherry',
+            'pastry' => 'yeast dough',
+            'baking_time' => 0,
+        ];
+
+        $bakedPie = [
+            'id' => 1,
+            'stuffing' => 'strawberry', // меняем параметр, который нельзя менять.
+                                        // в результате transitions не должна быть выполнена
+            'pastry' => 'yeast dough',
+            'baking_time' => 50,
+        ];
+
+        $transitionResult = $this->engine->makeTransition('new', 'baked', $bakedPie, $rawPie);
+        $this->assertFalse($transitionResult->isSuccess());
+    }
+
+    /**
+     * Проверка properties заданных в transitions
+     *
+     * @return false
+     */
+    public function testCookingBurntPie()
+    {
+        $rawPie = [
+            'id' => 1,
+            'stuffing' => 'cherry',
+            'pastry' => 'yeast dough',
+            'baking_time' => 0,
+        ];
+
+        $bakedPie = [
+            'id' => 1,
+            'stuffing' => 'cherry',
+            'pastry' => 'yeast dough',
+            'baking_time' => 60,    // при значениях более 50, пирог считается пригоревшим.
+                                    // transitions выполняться не должна
+        ];
+
+        $transitionResult = $this->engine->makeTransition('new', 'baked', $bakedPie, $rawPie);
+        $this->assertFalse($transitionResult->isSuccess());
+    }
+
+    /**
+     * Список всех возможных переходов из статуса new
+     * всего доступно 2 перехода
+     *
+     * @return true
+     */
+    public function testViewAllCookingState()
+    {
+        $rawPie = [
+            'id' => 1,
+            'stuffing' => 'cherry',
+            'pastry' => 'yeast dough',
+            'baking_time' => 0,
+        ];
+
+        $transitions = $this->engine->getAvailableStates('new', $rawPie);
+        $this->assertTrue(count($transitions) === 2);
     }
 }
